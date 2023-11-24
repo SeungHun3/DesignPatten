97678e809925f26e251bdf146714766eb9b6e4f9
seunghun3 <shlee4508@gmail.com>
2023-11-25 03:22:02 +0900

    컴포지트 빌더 // 정의, 구현 각각 구분하여 링킹작업필요 **구조상 복잡한 부분이 있어  메모하기위해 한곳에 작성함**

--- Patten/Patten/02_Builder.h
+++ Patten/Patten/02_Builder.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// 단순 빌더, 흐름식 빌더
 struct HtmlBuilder;
 
 struct HtmlElement
@@ -62,6 +63,7 @@ struct HtmlBuilder
 		HtmlElement e{ child_name,child_text };
 		root.elements.emplace_back(e); // 객체생성하지 않고 직접 집어넣기
 	}
+	// 흐름식 빌더
 	HtmlBuilder& add_Flow_child(string child_name, string child_text)
 	{
 		HtmlElement e{ child_name,child_text };
@@ -140,12 +142,12 @@ struct P : Tag
 {
 
 	explicit P(const string& text)
-		: Tag{"p", text}
+		: Tag{"p", text} // 명시적
 	{
 
 	}
 	P(initializer_list<Tag> children)
-		: Tag("p",children)
+		: Tag("p",children) // 자식개체가 들어갈 수 있으니 암시적
 	{
 
 	}
@@ -159,4 +161,164 @@ struct IMG : Tag
 		attributes.emplace_back("src", url);
 	}
 
+};
+
+
+// 컴포지트 빌더 == 객체 하나 생성시 복수의 빌더가 사용되는 경우 (언리얼 eos 시스템 구조)
+
+class PersonAddressBuilder;
+class PersonJobBuilder;
+
+// PersonBulider 통해 객체 생성하기
+class Person
+{
+public:
+	//주소
+	string street_address, post_code, city;
+	//직업
+	string company_name, position;
+	int annual_income = 0;
+
+
+	// 생성사 구분이유: 새로운 객체생성은 builder통해, 정보 담는 
+private:
+	Person(){}
+public:
+	static PersonBuilder create()
+	{
+		return PersonBuilder{};
+	}
+
+	Person(Person&& other)
+		: street_address{ move(other.street_address) },
+		post_code{ move(other.post_code) },
+		city{ move(other.city) },
+		company_name{ move(other.company_name) },
+		position{ move(other.position) },
+		annual_income{ other.annual_income }
+	{
+	}
+
+	Person& operator=(Person&& other)
+	{
+		if (this == &other)
+			return *this;
+		street_address = move(other.street_address);
+		post_code = move(other.post_code);
+		city = move(other.city);
+		company_name = move(other.company_name);
+		position = move(other.position);
+		annual_income = other.annual_income;
+		return *this;
+	}
+
+	friend class PersonBuilder;
+	friend class PersonAddressBuilder;
+	friend class PersonJobBuilder;
+};
+
+// PersonBuilder 의 Person객체를 참조받음
+class PersonBuilderBase
+{
+protected:
+	Person& person; // 생성중인 객체에 대한 참조만 가질 뿐 생성된 객체는 가지지 않는다
+	
+	explicit PersonBuilderBase(Person& person)
+		:person{ person }
+	{
+
+	}
+public:
+	/*
+	operator Person() 함수는 타입 변환 연산자로서, PersonBuilderBase 클래스의 객체를 Person 클래스로 암시적으로 변환해주는 함수
+	PersonBuilderBase builder(person); // person은 PersonBuilder를 통한 PersonBuilderBase의 생성자에서 전달된 Person 객체를 가리킴
+	==> Person person_instance = builder; // 이 순간 operator Person() 함수가 호출되어 PersonBuilderBase를 Person으로 변환
+	*/
+	operator Person()
+	{
+		// std::move 함수 : 주어진 객체를 다른 객체로 이동(객체의 소유권을 전환)시키는데 사용 (필요한 복사를 피하고 성능을 향상시킴)
+		return move(person); 
+	}
+
+	// 하위빌더의 인터페이스를 리턴
+	PersonAddressBuilder lives() const
+	{
+		return PersonAddressBuilder{ person };
+	}
+	PersonJobBuilder works() const
+	{
+		return PersonJobBuilder{ person };
+	}
+	// 하위 빌더에서 각각 주소, 직업정보를 초기화
+};
+
+// 생성하고 있는 객체 자체
+class PersonBuilder :public PersonBuilderBase
+{
+	Person p; // 생성중인 객체
+	
+public:
+	PersonBuilder() 
+		: PersonBuilderBase{p}
+	{
+	
+	}
+};
+
+//PersonBuilderBase 에서 파생된 각각의 빌더들 호출 
+// 하위빌더 호출할때마다 person에 대한 참조만 가진채로 각각의 다른빌더들이 값을 채워주는 형식 live().at().with_
+class PersonAddressBuilder :public PersonBuilderBase
+{
+	typedef PersonAddressBuilder Self;
+public:
+	explicit PersonAddressBuilder(Person& person)
+		: PersonBuilderBase{ person }
+	{
+	}
+
+	Self& at(std::string street_address)
+	{
+		person.street_address = street_address;
+		return *this;
+	}
+
+	Self& with_postcode(std::string post_code)
+	{
+		person.post_code = post_code;
+		return *this;
+	}
+
+	Self& in(std::string city)
+	{
+		person.city = city;
+		return *this;
+	}
+};
+
+class PersonJobBuilder : public PersonBuilderBase
+{
+	typedef PersonJobBuilder Self;
+public:
+	explicit PersonJobBuilder(Person& person)
+		: PersonBuilderBase{ person }
+	{
+	}
+
+	Self& at(std::string company_name)
+	{
+		person.company_name = company_name;
+		return *this;
+	}
+
+	Self& as_a(std::string position)
+	{
+		person.position = position;
+		return *this;
+	}
+
+	Self& earning(int annual_income)
+	{
+		person.annual_income = annual_income;
+		return *this;
+	}
 };
--- Patten/Patten/main.cpp
+++ Patten/Patten/main.cpp
@@ -47,8 +47,51 @@ int main()
 	cout << P{ IMG{ "http:aaaa"} } << endl;
 	cout << TagBuilder << endl;
 
+	// 컴포지트 빌더
+	Person p = Person::create()
+		.lives().at("gangnam").with_postcode("5413").in("seoul")
+		.works().as_a("mycompany").at("mycompany").earning(120);
+	/* 정보 담은 뒤
+	
+	PersonBuilderBase가 Person으로 형변환 될때
 
+	직접구현된 형변환 함수 
+	 operator Person() 
+	{
+		return move(person);
+	}
 
+	move함수 통해 person의 정보를 가진(이동시킨) 새로운 Person 객체(rValue)가 생성됨
 
+	=>
+	Person(Person&& other)
+		: street_address{ move(other.street_address) },
+		post_code{ move(other.post_code) },
+		city{ move(other.city) },
+		company_name{ move(other.company_name) },
+		position{ move(other.position) },
+		annual_income{ other.annual_income }
+	{
+	}
+	생성자 호출 후 
+	
+	Person p 에 대입할 대입연산
+
+	Person& operator=(Person&& other)
+	{
+		if (this == &other)
+			return *this;
+		street_address = move(other.street_address);
+		post_code = move(other.post_code);
+		city = move(other.city);
+		company_name = move(other.company_name);
+		position = move(other.position);
+		annual_income = other.annual_income;
+		return *this;
+	}
+
+	이 이루어짐
+
+	*/
 	return 0;
 }
